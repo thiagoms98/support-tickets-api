634bc8d9b420d2ddd93530f86ca9eff676e70788
Thiago Moreira <thiagoms98@outlook.com>
2025-08-22 13:52:08 -0300

    feat: Route to close ticket

---
+++ src/controllers/tickets/updateStatus.js
@@ -0,0 +1,7 @@
+export function updateStatus({ request, response, database }) {
+  const { id } = request.params;
+
+  database.update("tickets", id, { status: "closed" });
+
+  return response.end();
+}
